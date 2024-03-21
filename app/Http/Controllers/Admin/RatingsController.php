25b0a02cd355eb6bd2ff9d403c091f1b047a6e61
Divya Chaudhary <chaudharydivya125@gmail.com>
2024-03-21 23:15:33 +0530

    fix

--- app/Http/Controllers/Admin/RatingsController.php
+++ app/Http/Controllers/Admin/RatingsController.php
@@ -236,6 +236,8 @@ class RatingsController extends AppController
 		        if(!$validator->fails())
 		        {
 		        	unset($data['_token']);
+					$data['status'] = isset($data['status']) && $data['status'] ? $data['status'] : 0;
+					$data['image_status'] = isset($data['image_status']) && $data['image_status'] ? $data['image_status'] : 0;
 		        	if(Ratings::modify($id, $data))
 		        	{
 		        		$request->session()->flash('success', 'Rating updated successfully.');
