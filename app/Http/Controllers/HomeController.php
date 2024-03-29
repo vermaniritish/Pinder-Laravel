936a9f80d6dc675a47e250f00c95384bff282fcd
Divya Chaudhary <chaudharydivya125@gmail.com>
2024-03-29 22:26:13 +0530

    fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,11 +8,20 @@ use App\Models\Admin\Ratings;
 use App\Models\Admin\Sliders;
 use App\Models\Admin\ProductCategories;
 use App\Models\Admin\ProductSubCategories;
+use App\Models\Admin\Users;
 
 class HomeController extends BaseController
 {
     public function index(Request $request)
     {
+        $token = $request->query('token');
+        if ($token) {
+            $user = Users::where('token', $token)->first();
+            if ($user && is_null($user->email_verified_at)) {
+                $user->email_verified_at = now(); 
+                $user->save();
+            }
+        }
         $sliders = Sliders::where('status',1)->get();
         $testimonials = Ratings::where('status',1)->get();
         return view('frontend.home.index', ['sliders' => $sliders,'testimonials' => $testimonials]);
